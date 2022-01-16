fa64f43b7993be7dab77b785ef81572a230cc130
Tomas Holthe-Tveit <tomas.tveit@fortedigital.no>
2022-01-16 15:09:01 +0100

    Add TableB with reference to A.

---
+++ DatabaseProject/dbo/Tables/TableB.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TableB] (
+    [Id] INT NOT NULL,
+    [TableAId] int not null,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT FK_TableB_TableA FOREIGN KEY (TableAId) REFERENCES dbo.TableA(Id)
+);
+
