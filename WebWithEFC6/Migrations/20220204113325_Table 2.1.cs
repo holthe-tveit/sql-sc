11285068e0a6e2cfb5a7ed6418d33eab9f21bb2a
Tomas Eilev Holthe-Tveit <tomas.tveit@fortedigital.no>
2022-02-04 12:34:19 +0100

    Added Table2

---
+++ WebWithEFC6/Migrations/20220204113325_Table 2.1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebWithEFC6.Migrations
+{
+    public partial class Table21 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Table2",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Table2", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Table2");
+        }
+    }
+}
