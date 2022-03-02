24d2787eef99b363ab210dab075117b467533c25
Tomas Eilev Holthe-Tveit <tomas.tveit@fortedigital.no>
2022-03-02 21:10:57 +0100

    Removed powershell file no longer needed.

--- sql/runTSQLTests.ps1
+++
@@ -1,6 +0,0 @@
-
-Install-Module -Name SqlServer -Scope CurrentUser -Confirm
-Import-Module SqlServer
-
-$testResult= Invoke-Sqlcmd -Query "exec tSQLt.RunAll" -ConnectionString $ConnectionString -OutputAs DataSet -verbose -AbortOnError *> tSqlTestResult.txt
-return $testResult
