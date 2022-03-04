550366603ab5a35f4af5fd73705be3d50e39abfc
Tomas Eilev Holthe-Tveit <tomas.tveit@fortedigital.no>
2022-03-04 10:08:27 +0100

    Add new Tests

---
+++ sql/R__0003_TestSP.sql
@@ -0,0 +1,13 @@
+IF  exists(SELECT * from sys.procedures where name = 'TestTable4Insertion')
+BEGIN
+drop procedure MyTests.TestTable4Insertion
+end
+go
+create PROCEDURE MyTests.TestTable4Insertion
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+ exec tSQLt.AssertEquals '1', '1'
+END;
